ef30c3c76f04b488a548c3108f32ba4ffcb330cc
Eric Hildebrand <eric@blackantelope.com>
2024-10-03 15:48:45 -0700

    Add Google Analytics and set up in appjs

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import Layout from '../components/Layout'
 import '../styles/globals.css';
 import { AppContextProvider } from '../context/provider';
+import { GoogleAnalytics } from 'nextjs-google-analytics'
 
 export default function App({ Component, pageProps }) {
   return (
     <AppContextProvider>
       <Layout>
+        <GoogleAnalytics trackPageViews />
         <Component {...pageProps} />
       </Layout>
     </AppContextProvider>
